2eab8a5e2e4397623532d28b3aae8cab84b867bb
Wafaa Mohamed <121331529+Wafaamohamed2@users.noreply.github.com>
2024-09-24 15:10:57 +0300

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,105 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewEF
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+
+            options.UseSqlServer(@"Data Source=WAFAA;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+          
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+
+        public DbSet<Stock> Stock { get; set; }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<Nationality> Nationalities { get; set; }
+    }
+
+
+   
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public int BlogId { get; set; }
+
+        public string Title { get; set; }   
+
+        public string Content { get; set; }
+        public Blog Blog { get; set; }
+
+
+
+    }
+   
+    public class Blog 
+    {
+        public int BlogId { get; set; }
+
+        public string Url { get; set; }
+
+        public DateTime AddOn { get; set; } 
+        public List<Post> Posts { get; set; }   
+
+    }
+    public class Stock
+    {
+        public int id { get;  set; }
+
+        public string first_name { get; set; }
+
+        public string last_name { get; set; }
+
+        public string email { get; set; }
+
+       
+        public string gender { get; set; }
+    }
+    
+    public class Book {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public int AuthorId { get; set; }
+    }
+
+    public class Author
+    {
+        public int AuthorId { get; set; }
+
+        public string Name { get; set; }
+
+        public int NationalityId { get; set; }
+
+    }
+
+    public class Nationality
+    {
+        public int NationalityId { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewEF;
+using System.Net.WebSockets;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var _context = new ApplicationDbContext();
+
+
+        var stocks = GetData(1, 10);
+        foreach (var stock in stocks)
+            Console.WriteLine(stock.id);
+
+
+        Console.WriteLine("----------------Group by method--------------");
+
+        var stock2 = _context.Stock.GroupBy(m => m.gender)
+            .Select(m => new { Gender = m.Key, Count = m.Count() })
+            .OrderByDescending(m => m.Count);
+
+        foreach (var stock in stock2)
+            Console.WriteLine(stock.Gender + "-- " + stock.Count);
+
+
+        Console.WriteLine("--------------Joins-------------------");
+
+        // Inner Join
+
+        var books = _context.Books
+            .Join(
+              _context.Authors,
+              book => book.AuthorId,
+              author => author.AuthorId,
+              (book, author) => new
+              {
+                  BookId = book.Id,
+                  BookName = book.Name,
+                  AuthorName = author.Name,
+                  AuthorNationalityId = author.NationalityId
+
+              }
+
+
+            ).Join(
+                 _context.Nationalities,
+                 book => book.AuthorNationalityId,
+                 nationnality => nationnality.NationalityId,
+
+                   (book, nationality) => new
+                   {
+                      book.BookId,
+                      book.BookName,
+                      book.AuthorName,
+                      AuthorNationality = nationality.Name
+
+                   }
+
+
+            );
+
+        foreach (var book in books)
+            Console.WriteLine($"{book.BookId} -- {book.BookName} -- {book.AuthorName} -- {book.AuthorNationality}");
+
+
+
+        Console.WriteLine("------------------Tracking VS NoTracking-----------------");
+
+        //the value  of the price of the book with id 1 will change becous using of SaveChanges (Tracking)
+
+        var bookT = _context.Books.SingleOrDefault(b => b.Id == 1);
+        bookT.Price = 150;
+        _context.SaveChanges();
+
+
+        var bookNT = _context.Books.AsNoTracking().SingleOrDefault(b => b.Id == 1);
+        bookNT.Price = 110;
+        _context.SaveChanges();
+    }
+
+    public static List<Stock> GetData(int pagnum , int pagsize)
+    {
+
+        var _context = new ApplicationDbContext();
+
+        return _context.Stock.Skip((pagnum-1)*pagsize).Take(pagsize).ToList();
+
+    }
+}
